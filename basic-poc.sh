a74e712a5c6a3d65f7279c958edaf5b4af2564e1
Ankit Tyagi <ankittyaagi@gmail.com>
2023-04-29 12:49:52 +0530

    update

--- basic-poc.sh
+++ basic-poc.sh
@@ -9,6 +9,7 @@ fi
 # Variables
 INTERFACE=enp0s3
 LOG_FILE="/home/ubuntu/repo/OCI-never-idle/basic-poc.log"
+RUN_DURATION_SECONDS=300
 
 # Run stress-ng test
 stress_ng_output=$(stress-ng --cpu $(nproc) --io 1 --vm 1 --vm-bytes 1M --timeout ${RUN_DURATION_SECONDS}s --metrics)
