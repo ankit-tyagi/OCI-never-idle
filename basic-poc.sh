f1780c8ae6e0a515885a39fa011f084be41d44be
Ankit Tyagi <ankittyaagi@gmail.com>
2023-04-29 11:26:21 +0530

    update

--- basic-poc.sh
+++ basic-poc.sh
@@ -4,9 +4,9 @@
 
 # Variables
 CPU_PERCENT=25
-NET_PERCENT=15
+NET_SPEED_KBPS=200
 LOG_FILE="basic-poc.log"
-INTERFACE="eth0" # Replace with your network interface, e.g., wlan0, enp0s3, etc.
+INTERFACE="enp0s3" # Replace with your network interface, e.g., wlan0, enp0s3, eth0, etc.
 DURATION=3600 # Duration in seconds (1 hour)
 
 # Function to check if a command exists
@@ -14,11 +14,11 @@ command_exists () {
     type "$1" &> /dev/null ;
 }
 
-# Check if cpulimit, tc, and ethtool are installed
-if command_exists cpulimit && command_exists tc && command_exists ethtool; then
-    echo "cpulimit, tc, and ethtool are installed, continuing with the script." | tee -a "$LOG_FILE"
+# Check if cpulimit and tc are installed
+if command_exists cpulimit && command_exists tc; then
+    echo "cpulimit and tc are installed, continuing with the script." | tee -a "$LOG_FILE"
 else
-    echo "Error: cpulimit, tc, and/or ethtool not found. Please install them before running this script." | tee -a "$LOG_FILE"
+    echo "Error: cpulimit and/or tc not found. Please install them before running this script." | tee -a "$LOG_FILE"
     exit 1
 fi
 
@@ -28,15 +28,14 @@ CPULIMIT_PID=$!
 echo "CPU usage for this shell limited to $CPU_PERCENT% (cpulimit PID: $CPULIMIT_PID)" | tee -a "$LOG_FILE"
 
 # Limit network bandwidth usage
-MAX_BW=$(ethtool $INTERFACE | grep -i "speed" | awk '{print $2}') # Get maximum bandwidth in Mbps
-LIMIT_BW=$(echo "$MAX_BW * $NET_PERCENT / 100" | bc) # Calculate limit in Mbps
+LIMIT_BW=$NET_SPEED_KBPS
 
 # Set up traffic control
 tc qdisc add dev $INTERFACE root handle 1: htb default 10 &>> "$LOG_FILE"
-tc class add dev $INTERFACE parent 1: classid 1:1 htb rate ${LIMIT_BW}Mbit ceil ${LIMIT_BW}Mbit &>> "$LOG_FILE"
-tc class add dev $INTERFACE parent 1:1 classid 1:10 htb rate ${LIMIT_BW}Mbit ceil ${LIMIT_BW}Mbit &>> "$LOG_FILE"
+tc class add dev $INTERFACE parent 1: classid 1:1 htb rate ${LIMIT_BW}Kbit ceil ${LIMIT_BW}Kbit &>> "$LOG_FILE"
+tc class add dev $INTERFACE parent 1:1 classid 1:10 htb rate ${LIMIT_BW}Kbit ceil ${LIMIT_BW}Kbit &>> "$LOG_FILE"
 
-echo "Network bandwidth limited to $LIMIT_BW Mbps on interface $INTERFACE" | tee -a "$LOG_FILE"
+echo "Network bandwidth limited to $LIMIT_BW Kbps on interface $INTERFACE" | tee -a "$LOG_FILE"
 
 # Sleep for the duration
 sleep $DURATION
